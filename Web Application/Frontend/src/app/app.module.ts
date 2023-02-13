b672fffabcd7030897fb0fbae809edfc7cde4509
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-02-14 02:15:23 +0530

    added pages

--- Web Application/Frontend/src/app/app.module.ts
+++ Web Application/Frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './sharepages/header/header.component';
+import { FooterComponent } from './sharepages/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AccountComponent } from './pages/account/account.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PromotionComponent } from './pages/promotion/promotion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AccountComponent,
+    AboutComponent,
+    PromotionComponent
   ],
   imports: [
     BrowserModule,
