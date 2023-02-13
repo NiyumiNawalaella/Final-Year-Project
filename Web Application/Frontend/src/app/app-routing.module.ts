cdee33ead0a2b2f88e5c132742654cb27ad42f93
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-02-14 02:53:07 +0530

    changes made

--- Web Application/Frontend/src/app/app-routing.module.ts
+++ Web Application/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { AccountComponent } from './pages/account/account.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PromotionComponent } from './pages/promotion/promotion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'account', component:AccountComponent},
+  {path: 'promotion', component:PromotionComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
