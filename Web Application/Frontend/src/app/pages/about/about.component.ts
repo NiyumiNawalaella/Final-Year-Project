a7fa8683fc7337c11edc8ddd47c85ec619df8c40
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-03-08 20:09:42 +0530

    changes made

--- Web Application/Frontend/src/app/pages/about/about.component.ts
+++ Web Application/Frontend/src/app/pages/about/about.component.ts
@@ -6,38 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-//   images: any[] = [
-//     {
-//         previewImageSrc:
-// 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210203171024/CSSTutorial.png',
-//         thumbnailImageSrc:
-// 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210203171024/CSSTutorial.png',
-//         alt: 'Cascading Style Sheet',
-//         title: 'CSS'
-//     },
-//     {
-//         previewImageSrc:
-// 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210322182256/AngularJS-Tutorial.png',
-//         thumbnailImageSrc:
-// 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210322182256/AngularJS-Tutorial.png',
-//         alt: 'Angular for Front end',
-//         title: 'Angular'
-//     },
-//     {
-//         previewImageSrc:
-//             'https://media.geeksforgeeks.org/wp-content/cdn-uploads/Java.png',
-//         thumbnailImageSrc:
-//             'https://media.geeksforgeeks.org/wp-content/cdn-uploads/Java.png',
-//         alt: 'Java Programming Language',
-//         title: 'Java'
-//     },
-//     {
-//         previewImageSrc:
-// 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220401124017/HTML-Tutorial.png',
-//         thumbnailImageSrc:
-// 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220401124017/HTML-Tutorial.png',
-//         alt: 'HyperText Markup Language',
-//         title: 'HTML'
-//     },
-// ];
+  images: any[] = [
+    {
+        previewImageSrc:
+'../../../assets/images/testimonial/user/1.jpg',
+        thumbnailImageSrc:
+'../../../assets/images/testimonial/user/1.jpg',
+        comment: 'The facilities are well maintained. And the staff were warm welcoming and very helpful. I was very satisfied with the sport club services. I will be continuing my journey with them!',
+        name: 'Anna Blake'
+    },
+    {
+        previewImageSrc:
+'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210322182256/AngularJS-Tutorial.png',
+        thumbnailImageSrc:
+'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210322182256/AngularJS-Tutorial.png',
+        alt: 'Angular for Front end',
+        title: 'Angular'
+    },
+    {
+        previewImageSrc:
+            'https://media.geeksforgeeks.org/wp-content/cdn-uploads/Java.png',
+        thumbnailImageSrc:
+            'https://media.geeksforgeeks.org/wp-content/cdn-uploads/Java.png',
+        alt: 'Java Programming Language',
+        title: 'Java'
+    },
+    {
+        previewImageSrc:
+'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220401124017/HTML-Tutorial.png',
+        thumbnailImageSrc:
+'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220401124017/HTML-Tutorial.png',
+        alt: 'HyperText Markup Language',
+        title: 'HTML'
+    },
+];
 }
