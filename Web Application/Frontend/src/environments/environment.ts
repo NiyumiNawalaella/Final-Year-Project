0eca7371a3bb063002a1d78ef3e263fe0219e377
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-03-28 14:12:06 +0530

    firebase database added

--- Web Application/Frontend/src/environments/environment.ts
+++ Web Application/Frontend/src/environments/environment.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: false,
-  BASE_URL: '',
-  // API_URL: 'http://localhost:3000',
+  // BASE_URL: '',
+  // // API_URL: 'http://localhost:3000',
+  firebase : {
+    apiKey: "AIzaSyAjxYyfii13dwqgYxIzZpcvvUSmYSwW_qs",
+    authDomain: "shop-navigation-application.firebaseapp.com",
+    projectId: "shop-navigation-application",
+    storageBucket: "shop-navigation-application.appspot.com",
+    messagingSenderId: "130061722568",
+    appId: "1:130061722568:web:1ea048bdc73420d63d56fd",
+    measurementId: "G-ZLZYYS2W3E"
+  }
 };
