875fe1156ce15f7ca6ede3cda68c7530aba726bd
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-03-29 09:33:29 +0530

    changes made and the database added

--- Web Application/Frontend/src/app/pages/admin-account/admin-account.component.ts
+++ Web Application/Frontend/src/app/pages/admin-account/admin-account.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/shared/auth.service';
 
 @Component({
   selector: 'app-admin-account',
   templateUrl: './admin-account.component.html',
   styleUrls: ['./admin-account.component.css']
 })
-export class AdminAccountComponent {
+export class AdminAccountComponent implements OnInit {
 
+  email: string = "";
+  password: string = "";
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+
+  }
+  login() {
+    if(this.email=="")
+    {
+      alert("Please enter email");
+      return;
+    }
+    if(this.password=="")
+    {
+      alert("Please enter password");
+      return;
+    }
+
+    this.auth.admin_login(this.email, this.password);
+    this.email = "admin@kzone.ac.lk";
+    this.password = "1WePrOvidE56";
+  }
 }
--- Web Application/Frontend/src/app/pages/shopowner-account/shopowner-account.component.ts
+++ Web Application/Frontend/src/app/pages/shopowner-account/shopowner-account.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/shared/auth.service';
 
 @Component({
   selector: 'app-shopowner-account',
   templateUrl: './shopowner-account.component.html',
   styleUrls: ['./shopowner-account.component.css']
 })
-export class ShopownerAccountComponent {
+export class ShopownerAccountComponent implements OnInit {
+  email: string = "";
+  password: string = "";
 
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+
+  }
+  login() {
+    if(this.email=="")
+    {
+      alert("Please enter email");
+      return;
+    }
+    if(this.password=="")
+    {
+      alert("Please enter password");
+      return;
+    }
+
+    this.auth.shopowner_login(this.email, this.password);
+    this.email = "";
+    this.password = "";
+  }
 }
